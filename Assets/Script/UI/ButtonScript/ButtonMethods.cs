2979c93ea5043625e81c12818525fb4c334c686e
JFWind <1301763094@qq.com>
2024-08-15 13:34:11 +0800

    1

---
+++ Assets/Script/UI/ButtonScript/ButtonMethods.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonMethods : MonoBehaviour
+{
+    public void Test()
+    {
+        Debug.Log(1);
+    }
+}
